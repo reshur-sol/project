df239cff3b7f392e25cc0969e7d6f82eee50c76d
resur <sol.reshur@gmail.com>
2022-12-17 13:58:23 +0900

    background color changer

---
+++ 01.miniproject/01.BackgroundColorChanger/script.js
@@ -0,0 +1,5 @@
+const colorChangeBtn = () => {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randomColor;
+  color__code.innerHTML = "#" + randomColor;
+};
